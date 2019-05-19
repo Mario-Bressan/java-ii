c2dc694ea07335364cd75cd1ef85efc363441d68
Mario Bressan <mario.bressan@hotmail.com>
2019-05-18 22:31:55 -0300

    Mixing Generic and Nongeneric Collections.

---
+++ src/com/bressan/ocp/generic/GenericTest.java
@@ -0,0 +1,40 @@
+package com.bressan.ocp.generic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GenericTest {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>();
+
+        myList.add(2);
+        myList.add(4);
+        myList.add(6);
+
+        Adder adder = new Adder();
+        int total = adder.addAll(myList);
+        System.out.println(total);
+    }
+}
+
+class Adder {
+    int addAll(List list) throws IllegalArgumentException {
+        int total = 0;
+        /**
+         *  codigo anterior ao Java 5 (sem generics)
+         *  verifica se a collection e do tipo Integer
+         *  uma vez que nao era possivel especificar o tipo de objetos na lista
+         */
+        if ((list.size() > 0) && (list.get(0) instanceof Integer)) {
+            Iterator it = list.iterator();
+            while(it.hasNext()) {
+                int i = ((Integer)it.next()).intValue();
+                total += i;
+            }
+            return total;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
