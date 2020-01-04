cf67b89a5e4cd23f7d5667013aaa12a2454c4410
Mario Bressan <mario.bressan@hotmail.com>
2020-01-03 22:18:31 -0300

    Apply example

---
+++ src/com/bressan/ocp/functional/apply/ApplyExample.java
@@ -0,0 +1,24 @@
+package com.bressan.ocp.functional.apply;
+
+import java.util.function.Function;
+
+public class ApplyExample {
+    public static void main(String[] args) {
+        System.out.println(converter().apply(1).length());
+        System.out.println(converter().apply(300).length());
+        System.out.println(half().apply(2));
+        System.out.println(cube().apply(2));
+    }
+
+    static Function<Integer, String> converter() {
+        return (i) -> Integer.toString(i);
+    }
+
+    static Function<Integer, Double> half() {
+        return (i) -> i / 2.0;
+    }
+
+    static Function<Integer, Integer> cube() {
+        return (i) -> i*i*i;
+    }
+}
