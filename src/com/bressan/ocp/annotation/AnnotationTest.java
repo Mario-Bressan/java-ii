9062ca02731f005edafe26d2533c6b44f97f2460
mario <mario.bressan@hotmail.com>
2023-03-10 01:15:20 -0300

    teste feature

--- src/com/bressan/ocp/annotation/AnnotationTest.java
+++ src/com/bressan/ocp/annotation/AnnotationTest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class AnnotationTest {
     public static void main(String[] args) {
+        System.out.println("teste feature");
         User user = new User("mario", "123456789", LocalDate.of(1982, 01, 01));
         System.out.println(Validator.validate(user));
     }
