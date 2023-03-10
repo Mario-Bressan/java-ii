9ad6728dc0c388a71053812de7cfaf7270eae2ec
mario <mario.bressan@hotmail.com>
2023-03-10 01:24:22 -0300

    commit 1

--- src/com/bressan/ocp/annotation/AnnotationTest.java
+++ src/com/bressan/ocp/annotation/AnnotationTest.java
@@ -6,6 +6,7 @@ public class AnnotationTest {
     public static void main(String[] args) {
         System.out.println("teste feature");
         System.out.println("teste 3");
+        System.out.println("1");
         User user = new User("mario", "123456789", LocalDate.of(1982, 01, 01));
         System.out.println(Validator.validate(user));
     }
